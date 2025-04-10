15e1be31f350d944f9a0142d1d2b5840a27f6c9c
CratosCamilo <cratoscamilo@gmail.com>
2025-04-09 20:34:50 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import './styles/main.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const currentRoot = document.getElementById('root') as HTMLElement | null;
+if (!currentRoot) throw new Error('No existe el elemento root.');
+
+createRoot(currentRoot).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
